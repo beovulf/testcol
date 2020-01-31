9f2a82aa9a06d74cc77e6976c802a5943a072e42
beovulf <pcichon88@gmail.com>
2020-01-31 21:50:46 +0100

    Updates

---
+++ precache-manifest.f900432022d7d0aad097a600fdf4a600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5fd6ac3c4831422c75ee1d0124b51b",
+    "url": "/testcol/index.html"
+  },
+  {
+    "revision": "0b4f3f872f27416f5a27",
+    "url": "/testcol/static/css/2.3161ee17.chunk.css"
+  },
+  {
+    "revision": "086415e9d996c533df70",
+    "url": "/testcol/static/css/main.082c379f.chunk.css"
+  },
+  {
+    "revision": "0b4f3f872f27416f5a27",
+    "url": "/testcol/static/js/2.a01d15c2.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/testcol/static/js/2.a01d15c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "086415e9d996c533df70",
+    "url": "/testcol/static/js/main.86ce04b2.chunk.js"
+  },
+  {
+    "revision": "a35448b7aa55eb7a5788",
+    "url": "/testcol/static/js/runtime-main.5fd4dc1d.js"
+  }
+]);
